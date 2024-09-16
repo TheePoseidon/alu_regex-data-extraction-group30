908287c3a4c518afff53140530f71044b0f552e2
Maaxboon <maaxwelokoth@gmail.com>
2024-09-16 14:23:01 +0300

    Corrects indentation to follow PEP-8

--- phone_number.py
+++ phone_number.py
@@ -1,5 +1,6 @@
 import re
- phone_number_pattern = r"\d{3}-\d{3}-\d{4}|\(\d{3}\) \d{3}-\d{4}|\d{3}\.\d{3}\.\d{4}"
- def extract_phone_numbers(text):
- matches = re.findall(phone_number_pattern, text)
- return matches
+phone_number_pattern = r"\d{3}-\d{3}-\d{4}|\(\d{3}\) \d{3}-\d{4}|\d{3}\.\d{3}\.\d{4}"
+
+def extract_phone_numbers(text):
+    matches = re.findall(phone_number_pattern, text)
+    return matches
